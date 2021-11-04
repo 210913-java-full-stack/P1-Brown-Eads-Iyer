98affc16bf19d0a80105d208853f088f6125b2c8
James Brown <42588084+brownje3@users.noreply.github.com>
2021-11-04 00:17:23 -0400

    aws stuff

--- src/main/java/utils/hibernateManager.java
+++ src/main/java/utils/hibernateManager.java
@@ -30,6 +30,19 @@ public class hibernateManager {
 
     private static void configSession() {
         Configuration config = new Configuration().configure();
+        if(System.getProperty("TEST") == "TRUE") {
+            config.setProperty("hibernate.dialect", System.getProperty("HIBERNATE_DIALECT"));
+            config.setProperty("hibernate.connection.driver_class", System.getProperty("HIBERNATE_CONNECTION_DRIVER_CLASS"));
+            config.setProperty("hibernate.connection.url", System.getProperty("HIBERNATE_CONNECTION_URL"));
+            config.setProperty("hibernate.connection.username", System.getProperty("HIBERNATE_CONNECTION_USERNAME"));
+            config.setProperty("hibernate.connection.password", System.getProperty("HIBERNATE_CONNECTION_PASSWORD"));
+            config.setProperty("hibernate.hbm2ddl.auto", System.getProperty("HIBERNATE_HBM2DDL_AUTO"));
+            config.setProperty("hibernate.connection.autocommit", System.getProperty("HIBERNATE_CONNECTION_AUTOCOMMIT"));
+            config.setProperty("hibernate.show_sql", System.getProperty("HIBERNATE_SHOW_SQL"));
+            config.setProperty("hibernate.allow_update_outside_transaction", System.getProperty("HIBERNATE_ALLOW_OUTSIDE"));
+            config.setProperty("hibernate.connection.pool.size", System.getProperty("POOL_SIZE"));
+        }
+
         config.addAnnotatedClass(User.class);
         config.addAnnotatedClass(Booking.class);
         config.addAnnotatedClass(Flight.class);
